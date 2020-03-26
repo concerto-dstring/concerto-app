b6e884234dde3af1578439172f4a5eb0c0d01652
Tao <Tao@192.168.0.101>
2020-03-26 19:15:58 +0800

    addNewRow新增行方法优化（原方法新增行，新增数据只有一个属性“主题”。新增行数据应包含所有列，主题外其他列初始值为“”，否则界面修改列更新数据会导致找不到对于的属性）

--- src/maintable/MainTableDataStore.js
+++ src/maintable/MainTableDataStore.js
@@ -171,6 +171,12 @@ class MainTableDataStore {
         this._sizeRows ++;
         let id = this._sizeRows.toString();
         this._rowData[id] = {'1':newItem};
+        for(var i=0;i<this._columns.length;i++){
+            const columnKey = this._columns[i]['columnKey'];
+            if(i>2){
+                this._rowData[id][columnKey]='';
+            }
+        }
         let rows = this._groups[index].rows;
         rows.push(id);
 
