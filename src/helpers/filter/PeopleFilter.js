b01e25a0de0d23131500061e1c3db5e69b24acca
Tao <Tao@niejunchan.handhand.com>
2020-04-02 17:19:21 +0800

    人员过滤效果实现，过滤功能暂未实现，后续实现

--- src/helpers/filter/PeopleFilter.js
+++ src/helpers/filter/PeopleFilter.js
@@ -1,33 +1,74 @@
 import React from 'react';
 import 'antd/dist/antd.css'
 import { Popover, Button, Avatar } from 'antd';
-import {UserOutlined,} from '@ant-design/icons';
+import {UserOutlined,CloseCircleOutlined,SaveOutlined} from '@ant-design/icons';
 import './PeopleFilter.less'
 
 class PeopleFilter extends React.Component {
   constructor(props){
     super(props);
     this.state = {
+      visible:false,
+      hasCheckPeople:false,
       shape:'circle',
-      buttonText:'',
-      buttonType:'default'
+      buttonText:null,
+      buttonType:'default',
+      checkedPeople:{},
+      peopleAvatar:<UserOutlined />
     }
   }
   showPeople = () => {
     this.setState({
+      visible:true,
       shape:'round',
       buttonText:'Person',
       buttonType:'primary'
     })
   }
+  removePeople = () => {
+    this.setState({
+      visible:false,
+      hasCheckPeople:false,
+      shape:'circle',
+      buttonText:null,
+      buttonType:'default',
+      checkedPeople:{},
+      peopleAvatar:<UserOutlined />
+    });
+  }
+  checkPeople = (people) => {
+    const peopleAvatar = 
+      <Avatar 
+        size={30} 
+        className="peopleAvatarIcon"
+        style={{background:people.faceColor}}>
+        <span>{people.smallName}</span>
+      </Avatar>
+    this.setState({
+      visible:false,
+      shape:'round',
+      buttonText:null,
+      hasCheckPeople:true,
+      checkedPeople:people,
+      peopleAvatar:peopleAvatar
+    })
+  }
   visibleChange = (status) => {
-   if(!status){
+    if(!status&&!this.state.hasCheckPeople){
       this.setState({
         shape:'circle',
-        buttonText:'',
+        buttonText:null,
+        visible:false,
         buttonType:'default'
       })
-   }
+    }else if(!status&&this.state.hasCheckPeople){
+      this.setState({
+        shape:'round',
+        buttonText:null,
+        visible:false,
+        buttonType:'primary'
+      })
+    }
   }
   render = () => {
     const persons = [{
@@ -53,33 +94,54 @@ class PeopleFilter extends React.Component {
     }]
     const content = <div>
     {
-        persons.map((v, i) => (
-            <Button 
+      <div>
+         {
+           persons.map((people, i) => (
+             <Button 
                 shape="circle"
                 className="peopleAvatar"
+                onClick={this.checkPeople.bind(this,people)}
                 style={{
-                    background:v.faceColor
+                    background:people.faceColor,
+                    boxShadow:this.state.checkedPeople.userName == people.userName?'#cce9ff 0px 0px 5px 5px':'none',
                 }}
-            >
-            {v.smallName}
-            </Button>                 
-        ))
+             >
+             {people.smallName}
+             </Button>
+           ))
+         }
+         <Button 
+           className="saveView"
+           disabled={true}>
+           <SaveOutlined className="saveViewBtn"/>保存视图
+         </Button> 
+      </div>                
+        
     }
     </div>;
+    const removeIconStyle = {
+        display:this.state.hasCheckPeople?'inline':'none'
+    }
     return(
         <Popover 
             placement="bottom"
             trigger="click" 
             content={content} 
+            visible={this.state.visible}
             onVisibleChange={this.visibleChange}
         >
             <Button 
                 type={this.state.buttonType}
                 shape={this.state.shape}
-                icon={<UserOutlined />}
+                icon={this.state.peopleAvatar}
                 onClick={this.showPeople}
             >
-            {this.state.buttonText}    
+                {this.state.buttonText}
+                <CloseCircleOutlined 
+                    style={removeIconStyle} 
+                    className="removeIcon"
+                    onClick={this.removePeople}
+                />
             </Button>
         </Popover>
     )
