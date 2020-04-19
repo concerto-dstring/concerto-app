b216d6332d5e520df0ef59910c76e592ae76da84
shifengzhang <shifengzhang@users.noreply.github.com>
2020-04-18 18:02:06 -0600

    Revert "feature: add initial graphql client codes (not integrated with current"
    This reverts commit 89a47f11f7bdde64f45234ecfd2eea7aad4f659d.

--- src/App.js
+++ src/App.js
@@ -5,33 +5,14 @@
   "use strict";
 
   import React from 'react';
-  import appSyncConfig from "./aws-exports";
-  import { ApolloProvider } from "react-apollo";
-  import AWSAppSyncClient, { defaultDataIdFromObject } from "aws-appsync";
-  import { Rehydrated } from "aws-appsync-react";
-
+  
   import MainPage from './MainPage.js'
   
-  const client = new AWSAppSyncClient({
-    url: appSyncConfig.aws_appsync_graphqlEndpoint,
-    region: appSyncConfig.aws_appsync_region,
-    auth: {
-      type: appSyncConfig.aws_appsync_authenticationType,
-      apiKey: appSyncConfig.aws_appsync_apiKey,
-    },
-    cacheOptions: {
-    }
-  });
-
   class App extends React.Component {
   
     render() {
       return (
-        <ApolloProvider client={client}>
-          <Rehydrated>
-              <MainPage />
-          </Rehydrated>
-        </ApolloProvider> 
+          <MainPage />
       );
     }
   }
