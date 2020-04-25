2dda3d9d279de88a1eae1b02c9b78e1b2c506724
Shifeng Zhang <emailsf@gmail.com>
2020-04-25 16:00:28 -0600

    graphql related checkin
    1. add the new graphql-tag package
    2. add the sample code to access the dev backend graphql api (src/helpers/data)
    3. add createdAt fields to schema most tables
    4. pudate readme

--- src/App.js
+++ src/App.js
@@ -9,9 +9,13 @@
   import { ApolloProvider } from "react-apollo";
   import AWSAppSyncClient, { defaultDataIdFromObject } from "aws-appsync";
   import { Rehydrated } from "aws-appsync-react";
+  import gql from "graphql-tag";
 
   import MainPage from './MainPage.js'
   
+  import { listCompanys } from "./graphql/queries"
+
+  /* AWSAppSyncClient integrates with the Apollo Client for GraphQL */
   const client = new AWSAppSyncClient({
     url: appSyncConfig.aws_appsync_graphqlEndpoint,
     region: appSyncConfig.aws_appsync_region,
@@ -22,6 +26,14 @@
     cacheOptions: {
     }
   });
+
+  /* a sample codes to use the client */
+  console.log("start to run gql " + new Date().toISOString())
+  client
+    .query({
+      query: gql(listCompanys)
+    })
+    .then(result => console.log(result));
 
   class App extends React.Component {
   
