07f1d86536a4b9f489a589428ea4d5430e6df901
Shifeng Zhang <emailsf@gmail.com>
2020-05-02 22:35:35 -0600

    amplify auth auto-gen codes / config commit

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -6,7 +6,12 @@ const awsmobile = {
     "aws_appsync_graphqlEndpoint": "https://xebve4g6nbccfme6fj74xwcf4a.appsync-api.ap-northeast-2.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-imeyvtpjmvczlcv65vilfpshfy"
+    "aws_appsync_apiKey": "da2-imeyvtpjmvczlcv65vilfpshfy",
+    "aws_cognito_identity_pool_id": "ap-northeast-2:80801c94-1ad5-4a54-939c-22f047de45dd",
+    "aws_cognito_region": "ap-northeast-2",
+    "aws_user_pools_id": "ap-northeast-2_6oseBcIaX",
+    "aws_user_pools_web_client_id": "45ea5274ugc2qa3vecked0bq6e",
+    "oauth": {}
 };
 
 
