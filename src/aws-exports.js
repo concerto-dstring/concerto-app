393bfcd579175f6896af708c04ded1ca7578a3d3
Shifeng Zhang <emailsf@gmail.com>
2020-06-01 23:53:18 -0600

    product environment update

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,10 +3,15 @@
 
 const awsmobile = {
     "aws_project_region": "ap-northeast-2",
-    "aws_appsync_graphqlEndpoint": "https://43zsjvmtsfefjfnworauobwsyi.appsync-api.ap-northeast-2.amazonaws.com/graphql",
+    "aws_appsync_graphqlEndpoint": "https://fjuypof5xrctpbmtjfm4zx5c6a.appsync-api.ap-northeast-2.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-7izcwncuarccjfw5qdapfrzccu"
+    "aws_appsync_apiKey": "da2-5fuapofitfalfgo4f4a3pocz5q",
+    "aws_cognito_identity_pool_id": "ap-northeast-2:f1455023-9123-4838-a23f-24a32199b85f",
+    "aws_cognito_region": "ap-northeast-2",
+    "aws_user_pools_id": "ap-northeast-2_ijdyPKx2y",
+    "aws_user_pools_web_client_id": "45990m60nt54a49oqa5q6sl9di",
+    "oauth": {}
 };
 
 
