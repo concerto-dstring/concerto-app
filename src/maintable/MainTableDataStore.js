c921766737e1be6d03d5dc76d7c10b7428c37c3a
Ying Ma <>
2020-06-10 10:59:46 -0400

    Fixed pointer event bug

--- src/maintable/MainTableDataStore.js
+++ src/maintable/MainTableDataStore.js
@@ -998,7 +998,7 @@ class MainTableDataStore {
           variables: {
             input: {
               name: newItem,
-              columntype: ColumnType.EDITBOX,
+              columntype: ColumnType,
               columnComponentType: columnComponentType,
               createdAt: new Date().toISOString(),
               creatorID: this._currentUser.id,
